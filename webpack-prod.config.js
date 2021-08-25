af874c9714d17c1f93159a333926a6dcda6aabda
Opal Thongpetch <crystalclear506@gmail.com>
2021-08-25 16:19:54 +0700

    webpack configuration change

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    "mode": "development",
+    "mode": "production",
     "entry": [
         "./src/index.js",
     ],
